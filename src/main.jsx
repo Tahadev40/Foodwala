d699a63a01c91f501839d91ae0dba8c99de4a832
Taha Khan <tahanawazkhan555@gmail.com>
2025-08-28 01:13:50 +0500

    Downgrade to React 18 for helmet compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet';
 import './index.css';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
